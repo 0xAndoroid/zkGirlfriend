aab841fdef0dfc8f2f0490c098e4755259c0a2cf
Wes Jorgensen <wjorgensen29@gmail.com>
2024-08-16 14:15:43 -0700

    Finished SP1 intergration

--- contracts/src/girlfriendBank.sol
+++ contracts/src/girlfriendBank.sol
@@ -1,4 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 import {GirlfriendNFT} from "./girlfriendNFT.sol";
+import {ISP1Verifier} from "../lib/sp1-contracts/contracts/src/ISP1Verifier.sol";
 
 contract GirlfriendBank{
     address[] s_nfts;
@@ -6,6 +10,8 @@ contract GirlfriendBank{
     uint256 s_subscriptionPeriod;
 
     uint256 immutable paymentAmount;
+    address immutable SP1_VERIFIER;
+    bytes32 immutable VERIFICATION_KEY;
 
     error notEnoughPayment();
 
@@ -16,12 +22,15 @@ contract GirlfriendBank{
         _;
     }
 
-    constructor(uint256 _paymentAmount, uint256 _subscriptionPeriod){
+    constructor(uint256 _paymentAmount, uint256 _subscriptionPeriod, address _sp1verifier, bytes32 _verificationKey){
         paymentAmount = _paymentAmount;
         s_subscriptionPeriod = _subscriptionPeriod;
+        SP1_VERIFIER = _sp1verifier;
+        VERIFICATION_KEY = _verificationKey;
     } 
 
-    function newGirlfriend() external payable checkPaymentAmount {
+    function newGirlfriend(bytes calldata _publicValues, bytes calldata _proofBytes) external payable checkPaymentAmount {
+        _verifyProof(_publicValues, _proofBytes);
         GirlfriendNFT temp = new GirlfriendNFT(s_subscriptionPeriod, msg.sender);
         s_addressToArrayIndex[msg.sender] = s_nfts.length;
         s_nfts.push(address(temp));
@@ -31,4 +40,7 @@ contract GirlfriendBank{
         GirlfriendNFT(s_nfts[s_addressToArrayIndex[msg.sender]]).paySubscription();
     }
 
+    function _verifyProof(bytes calldata _publicValues, bytes calldata _proofBytes) internal view {
+        ISP1Verifier(SP1_VERIFIER).verifyProof(VERIFICATION_KEY, _publicValues, _proofBytes);
+    }
 }
